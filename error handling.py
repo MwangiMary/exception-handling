49a3a564f74a4fa30250b778542cf74d7035198b
MwangiMary <mary99mwangi@gmail.com>
2025-08-22 21:32:56 +0300

    Updated the txt file

--- error handling.py
+++ error handling.py
@@ -1,15 +1,16 @@
 def file_read_write():
     try:
-        # Just a simple risky operation
-        with open("output file.py", "r", encoding="utf-8") as f:
+        # Open "outputfile.txt"
+        with open("outputfile.txt", "r", encoding="utf-8") as f:
             content = f.read()
 
+        # Print the contents
         print(content)
 
     except FileNotFoundError:
-        print(" File not found.")
+        print("❌ File not found.")
     except Exception as e:
-        print(f" Unexpected error: {e}")
+        print(f"⚠️ Unexpected error: {e}")
 
 
 file_read_write()
